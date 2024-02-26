caffe6b73e820100cad639942349d8a9bc138c20
Zhuk, Anton <Anton.Zhuk@thomsonreuters.com>
2024-02-26 22:10:11 +0400

    add csv file with basic example; add basic console output

---
+++ Simplex/Simplex/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Welcome to simplex method console!\n");
+
+string[] textLines = File.ReadAllLines("Examples/2x2.csv");
+
+Console.WriteLine("Function to optimize:");
+Console.WriteLine(textLines[0]);
+
+Console.WriteLine("Conditions:");
+
+for (int i = 1; i < textLines.Length; i++)
+{
+	Console.WriteLine(textLines[i]);
+}
+
+Console.ReadLine();
