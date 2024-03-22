a66662ece69f60dce7ebc0765c6a995d5c9b3582
Zhuk, Anton <Anton.Zhuk@thomsonreuters.com>
2024-03-22 21:45:00 +0400

    add draft funcition to pretty-print restriction inequalities

--- Simplex/Simplex/Program.cs
+++ Simplex/Simplex/Program.cs
@@ -8,11 +8,13 @@ Console.WriteLine("Function to optimize:");
 
 Console.WriteLine(BuildFunctionToOptimizeMessage(textLines[0]));
 
+Console.WriteLine();
+
 Console.WriteLine("Conditions:");
 
-for (int i = 1; i < textLines.Length; i++)
+for (int i = 2; i < textLines.Length; i++)
 {
-	Console.WriteLine(textLines[i]);
+	Console.WriteLine(BuildRestrictionMessage(textLines[i]));
 }
 
 Console.ReadLine();
@@ -30,6 +32,23 @@ string BuildFunctionToOptimizeMessage(string functionTextLine)
 	}
 
 	sb.Append(" -> Max");
+
+	return sb.ToString();
+}
+
+string BuildRestrictionMessage(string restrictionTextLine)
+{
+	string[] coefficients = restrictionTextLine.Split([',']);
+
+	StringBuilder sb = new();
+
+	for (int i = 0;  i < coefficients.Length - 1; i++)
+	{
+		string sign = coefficients[i][0] == '-' ? "" : "+";
+		sb.Append($"{sign}{coefficients[i]}*x{i + 1}");
+	}
+
+	sb.Append($" <= {coefficients[coefficients.Length - 1]}");
 
 	return sb.ToString();
 }
