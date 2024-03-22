e17e885b0eebaf4d6ad3fc22ec6d74b3cf15e6bd
Zhuk, Anton <Anton.Zhuk@thomsonreuters.com>
2024-03-22 21:28:19 +0400

    add draft function to pretty-print function to optimize

--- Simplex/Simplex/Program.cs
+++ Simplex/Simplex/Program.cs
@@ -1,9 +1,12 @@
-﻿Console.WriteLine("Welcome to simplex method console!\n");
+﻿using System.Text;
+
+Console.WriteLine("Welcome to simplex method console!\n");
 
 string[] textLines = File.ReadAllLines("Examples/2x2.csv");
 
 Console.WriteLine("Function to optimize:");
-Console.WriteLine(textLines[0]);
+
+Console.WriteLine(BuildFunctionToOptimizeMessage(textLines[0]));
 
 Console.WriteLine("Conditions:");
 
@@ -13,3 +16,20 @@ for (int i = 1; i < textLines.Length; i++)
 }
 
 Console.ReadLine();
+
+string BuildFunctionToOptimizeMessage(string functionTextLine)
+{
+	string[] coefficients = functionTextLine.Split([',']);
+
+	StringBuilder sb = new($"F = {coefficients[0]}*x1");
+
+	for (int i = 1; i < coefficients.Length; i++)
+	{
+		string sign = coefficients[i][0] == '-' ? "" : "+";
+		sb.Append($"{sign}{coefficients[i]}*x{i + 1}");
+	}
+
+	sb.Append(" -> Max");
+
+	return sb.ToString();
+}
